9ef90cc88e8c418dec289e8de6835468fd7f9a6a
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-11-04 00:33:32 +0100

    files

--- backend/src/api.py
+++ backend/src/api.py
@@ -195,13 +195,27 @@ def unprocessable(error):
 
 '''
 
+
 '''
 @TODO implement error handler for 404
     error handler should conform to general task above
 '''
 
-
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 404
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+@app.errorhandler(AuthError)
+def handle_auth(e):
+    return jsonify({
+        "success": False,
+        "error": e.status_code,
+        'message': e.error
+    }), 401
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'dev-uk3m9v4d.eu.auth0.com', // the auth0 domain prefix
+    audience: 'coffe', // the audience set for the auth0 app
+    clientId: 'lG8UrDPj2IvMUaykRlFRyaVi0vRoUVRW', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
